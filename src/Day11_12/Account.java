933160a8a3e0f11e0cf68f7d34de931998670378
dhanrajpandagale <dhanrajpandagale@gmail.com>
2022-07-13 09:20:43 -0400

    Added: Account Debit Program

---
+++ src/Day11_12/Account.java
@@ -0,0 +1,26 @@
+package Day11_12;
+
+import java.util.Scanner;
+
+public class Account {
+    int accountBalance = 192000;
+
+    public static void main(String[] args) {
+        Account obj = new Account();
+        obj.debit();
+    }
+
+    void debit() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Amount You Want To Debit");
+        int amount = sc.nextInt();
+        if (amount <= accountBalance) {
+            accountBalance -= amount;
+            System.out.println("Your account is debited by RS " + amount + " and Available Balance is RS " + accountBalance);
+        } else {
+            System.out.println("Debit amount exceeded account Balance and Available Balance is RS" + accountBalance);
+        }
+
+    }
+
+}
